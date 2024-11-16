d4c21f100918a016b6bd4745d7a2add3cbb21db0
turtle505 <yoobin505@gmail.com>
2024-11-16 15:12:08 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 64 ms, Memory: 64432 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+int lcs(std::string text1, std::string text2);
+
+int main() {
+
+    std::string text1, text2;
+
+    std::cin >> text1 >> text2;
+    
+    std::cout << lcs(text1, text2);
+
+    return 0;
+}
+
+
+int lcs(std::string text1, std::string text2) {
+
+    int text1_len = text1.length();
+    int text2_len = text2.length();
+
+    int dp[text1_len+1][text2_len+1] = {};
+
+    int max_length = 0;
+
+    for(int i=1; i<text1_len+1; i++) {
+
+        for(int j=1; j<text2_len+1; j++) {
+            if(text1.at(i-1) == text2.at(j-1)) {
+                dp[i][j] = dp[i-1][j-1] + 1;
+                if (dp[i][j] > max_length) max_length = dp[i][j];
+            } else {
+                dp[i][j] = 0;
+            }
+
+        }
+
+    }
+
+    return max_length;
+
+}
