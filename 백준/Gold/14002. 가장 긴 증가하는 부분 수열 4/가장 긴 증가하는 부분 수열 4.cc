3fae028a8a962625554f9f52ce5c82dc0b9b4c06
Yoobeen <yoobin505@gmail.com>
2024-12-18 00:30:56 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n] = {};
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int cp[n] = {};
+    fill(cp, cp+n, 1);
+
+    int max_length = 0;
+    int max_idx = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++) {
+            if (arr[j] < arr[i]) cp[i] = max(cp[j]+1, cp[i]);
+        }
+
+        if (max_length < cp[i]) {
+            max_length = cp[i];
+            max_idx = i;
+        }
+    }
+
+    cout << max_length << '\n';
+    
+    vector<int> tmp;
+
+    for (int i=n-1; i>=0; i--) {
+        if(cp[i] == max_length) {
+            tmp.insert(tmp.begin(), arr[i]);
+            max_length--;
+        }
+    }
+
+    for (int e : tmp) {
+        cout << e << ' ';
+    }
+
+    return 0;
+}
