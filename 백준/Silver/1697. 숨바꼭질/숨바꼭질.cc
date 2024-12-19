1a3efd28e4dcf6001cfb44acd7d1a116bdfcbf2b
Yoobeen <yoobin505@gmail.com>
2024-12-20 02:40:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 4 ms, Memory: 3332 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+ 
+    int n, k;
+
+    cin >> n >> k;
+
+    queue<int> Q;
+
+    int visit[300000] = {};
+
+    Q.push(n);
+    visit[n] = 1;
+    visit[k] = -1;
+
+    if (n == k) {
+        cout << 0;
+        return 0;
+    }
+
+    while (!Q.empty()) {
+        int cur = Q.front();
+        Q.pop();
+
+        int nx[3] = {cur - 1, cur + 1, cur * 2};
+
+        for (int i=0; i<3; i++) {
+            if (nx[i] < 0 || nx[i] >= 300000 || visit[nx[i]] > 0)  continue;
+       
+            if (visit[nx[i]] == -1) {
+                cout << visit[cur];
+                return 0;
+            } 
+
+            visit[nx[i]] = visit[cur] + 1;
+            Q.push(nx[i]);
+        }
+    }
+
+    cout << "fail";
+
+    return 0;
+}
