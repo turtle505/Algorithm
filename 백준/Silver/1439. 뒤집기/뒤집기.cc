1553022f67e74682ac10bf492ce34a1d265ca116
Yoobeen <yoobin505@gmail.com>
2024-12-26 16:03:30 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -12,25 +12,23 @@ int main(void){
     int n = str.size();
     int m[n];
 
-
     for (int i=0; i<n; i++) {
         m[i] = str[i] - '0';
     }
 
-    int num_0 = 0;
-    int num_1 = 0;
+    int count[2] = {};
 
-    if(m[0] == 1) num_0++;
-    else num_1++;
+    count[m[0]]++;
 
     for (int i=1; i<n; i++) {
         if (m[i-1] != m[i]) {
-            if (m[i-1] == 0) num_0++;
-            else num_1++;
+            count[m[i]]++;
         }
     }
 
-    cout << min(num_0, num_1);
+    int result = count[0] > count[1]? count[1]:count[0];
+
+    cout << result;
 
     return 0;
 }
