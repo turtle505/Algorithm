07c6620dedc22e89c8db0e4911bd6cf7528f56e5
Yoobeen <yoobin505@gmail.com>
2024-12-27 23:18:54 +0900

    [Silver II] Title: N과 M (11), Time: 324 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int numbers[10];
+bool isused[10];
+int number_count[10001];
+int arr[8];
+
+void func(int k, int next) {
+    if (k == m) {
+        for (int i=0; i<m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i=0; i<10001; i++) {
+        if (number_count[i] != 0) {
+            arr[k] = i;
+            func(k+1, i);
+        }
+        
+    }
+}
+
+int main() {
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+ 
+    cin >> n >> m;
+
+    for (int i=0; i<n; i++) {
+        cin >> numbers[i];
+        number_count[numbers[i]]++;
+    }
+
+    sort(numbers, numbers+n);
+
+    func(0, 0);
+     
+    return 0;
+}
